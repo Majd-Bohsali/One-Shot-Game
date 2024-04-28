ed6f3c8d1be6220fbb500a3112c9c85e1b21c19e
Majd Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2024-04-28 12:08:57 -0700

    Add files via upload

---
+++ game/game/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bulletUpTimer = new System.Windows.Forms.Timer(this.components);
+            this.bird1Timer = new System.Windows.Forms.Timer(this.components);
+            this.collisionTimer = new System.Windows.Forms.Timer(this.components);
+            this.bulletDownTimer = new System.Windows.Forms.Timer(this.components);
+            this.checkForWinOrLossTimer = new System.Windows.Forms.Timer(this.components);
+            this.bird2Timer = new System.Windows.Forms.Timer(this.components);
+            this.aITimer = new System.Windows.Forms.Timer(this.components);
+            this.countTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // bulletUpTimer
+            // 
+            this.bulletUpTimer.Interval = 25;
+            this.bulletUpTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bird1Timer
+            // 
+            this.bird1Timer.Enabled = true;
+            this.bird1Timer.Interval = 50;
+            this.bird1Timer.Tick += new System.EventHandler(this.bird1Timer_Tick);
+            // 
+            // collisionTimer
+            // 
+            this.collisionTimer.Enabled = true;
+            this.collisionTimer.Interval = 1;
+            this.collisionTimer.Tick += new System.EventHandler(this.collisionTimer_Tick);
+            // 
+            // bulletDownTimer
+            // 
+            this.bulletDownTimer.Interval = 25;
+            this.bulletDownTimer.Tick += new System.EventHandler(this.bulletDownTImer_Tick);
+            // 
+            // checkForWinOrLossTimer
+            // 
+            this.checkForWinOrLossTimer.Enabled = true;
+            this.checkForWinOrLossTimer.Interval = 1;
+            this.checkForWinOrLossTimer.Tick += new System.EventHandler(this.checkForWin_Tick);
+            // 
+            // bird2Timer
+            // 
+            this.bird2Timer.Enabled = true;
+            this.bird2Timer.Interval = 50;
+            this.bird2Timer.Tick += new System.EventHandler(this.bird2Timer_Tick);
+            // 
+            // aITimer
+            // 
+            this.aITimer.Interval = 1;
+            this.aITimer.Tick += new System.EventHandler(this.aITimer_Tick);
+            // 
+            // countTimer
+            // 
+            this.countTimer.Interval = 50;
+            this.countTimer.Tick += new System.EventHandler(this.countTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 594);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer bulletUpTimer;
+        private System.Windows.Forms.Timer bird1Timer;
+        private System.Windows.Forms.Timer collisionTimer;
+        private System.Windows.Forms.Timer bulletDownTimer;
+        private System.Windows.Forms.Timer checkForWinOrLossTimer;
+        private System.Windows.Forms.Timer bird2Timer;
+        private System.Windows.Forms.Timer aITimer;
+        private System.Windows.Forms.Timer countTimer;
+    }
+}
+
---
+++ game/game/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Rectangle playerBox;
+        Rectangle bullet;
+        Rectangle bird1;
+        Rectangle bird2;
+        Rectangle wire1;
+        Rectangle wire2;
+        Rectangle bounceBackBlock1;
+
+        int numBirdsLeft = 2;
+        string moveDirectionForFunction = "left";
+        string moveDirectionForBird1 = "left";
+        string moveDirectionForBird2 = "left";
+        bool bird1IsAlive = true;
+        bool bird2IsAlive = true;
+        string bird1Direction;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playerBox = new Rectangle(300, 570, 30, 30);
+            bullet = new Rectangle(310, 570, 10, 10);
+            bird1 = new Rectangle(300, 300, 20, 20);           
+            bird2 = new Rectangle(300, 250, 20, 20);
+            wire1 = new Rectangle(0, 320, 600, 3);
+            wire2 = new Rectangle(0, 270, 600, 3);
+            bounceBackBlock1 = new Rectangle(150, 250, 40, 40);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.Blue), playerBox);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Gray), bullet);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Red), bird1);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Red), bird2);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Black), wire1);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Black), wire2);
+            e.Graphics.FillRectangle(new SolidBrush(Color.DarkGray), bounceBackBlock1);
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void moveBird(int minX, int maxX, Rectangle bird)
+        {
+            if (true)
+            {
+                if (moveDirectionForFunction == "left")
+                {
+                    bird.X -= 10;
+                    if (bird.X < minX)
+                    {
+                        moveDirectionForFunction = "right";
+                    }
+                    Refresh();
+                }
+                else if (moveDirectionForFunction == "right")
+                {
+                    bird.X += 10;
+                    if (bird.X > maxX)
+                    {
+                        moveDirectionForFunction = "left";
+                    }
+                    Refresh();
+                }
+
+
+            }
+        }
+        int count;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+   
+            count++;
+            bullet.Y -= 10;
+            if (count == 1)
+            {
+                count++;
+            }
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                left();
+                
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                right();
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                shoot();
+            }
+        }
+        int birdLocation;
+        void shoot()
+        {
+            birdLocation = bird2.X;
+            bulletUpTimer.Enabled = true;
+            countTimer.Enabled = true;
+        }
+        void left()
+        {
+            playerBox.X -= 10;
+            if (!bulletUpTimer.Enabled)
+            {
+                bullet.X -= 10;
+            }
+            Refresh();
+        }
+        void right()
+        {
+            playerBox.X += 10;
+            if (!bulletUpTimer.Enabled)
+            {
+                bullet.X += 10;
+            }
+            Refresh();
+        }
+        int count2;
+        private void bird1Timer_Tick(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                if (bulletUpTimer.Enabled==true)
+                {
+                    count2++;
+                }
+                if (moveDirectionForBird1 == "left")
+                {
+                    bird1.X -= 10;
+                    if (bird1.X < 10)
+                    {
+                        moveDirectionForBird1 = "right";
+                    }
+                    Refresh();
+                }
+                else if (moveDirectionForBird1 == "right")
+                {
+                    bird1.X += 10;
+                    if (bird1.X > 585)
+                    {
+                        moveDirectionForBird1 = "left";
+                    }
+                    Refresh();
+                }
+            }
+        }
+
+        private void collisionTimer_Tick(object sender, EventArgs e)
+        {
+            if (bullet.IntersectsWith(bird1))
+            {
+                bird1.X = 100000;
+                bird1IsAlive = false;
+                numBirdsLeft--;
+            }
+            else if(bullet.IntersectsWith(bird2))
+            {
+                bird2.X = 100000;
+                bird2IsAlive = false;
+                numBirdsLeft--;
+            }
+            else if(bullet.IntersectsWith(bounceBackBlock1))
+            {
+                bulletDownTimer.Enabled = true;
+
+            }
+            else if (bullet.IntersectsWith(playerBox))
+            {
+  
+                if(bulletDownTimer.Enabled)
+                {
+                    bulletUpTimer.Enabled = false;
+                    bulletDownTimer.Enabled = false;
+                }
+                
+            }
+        }
+
+        private void bulletDownTImer_Tick(object sender, EventArgs e)
+        {
+            bullet.Y += 20;
+            Refresh();
+        }
+
+        private void checkForWin_Tick(object sender, EventArgs e)
+        {
+           
+            if (numBirdsLeft == 0)
+            {
+                checkForWinOrLossTimer.Enabled = false;
+                MessageBox.Show("You Win");
+            }
+            else if (bullet.Y <= 0)
+            {
+                checkForWinOrLossTimer.Enabled = false;
+                MessageBox.Show("You Lose");
+                this.Close();
+            }
+        }
+
+        private void bird2Timer_Tick(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                if (moveDirectionForBird2 == "left")
+                {
+                    bird2.X -= 10;
+                    if (bird2.X < 250)
+                    {
+                        moveDirectionForBird2 = "right";
+                    }
+                    Refresh();
+                }
+                else if (moveDirectionForBird2 == "right")
+                {
+                    bird2.X += 10;
+                    if (bird2.X > 585)
+                    {
+                        moveDirectionForBird2 = "left";
+                    }
+                    Refresh();
+                }
+            }
+        }
+
+        private void aITimer_Tick(object sender, EventArgs e)
+        {
+            if (bird1IsAlive)
+            {
+                if(playerBox.X!=bounceBackBlock1.X)
+                {
+                    left();
+                }
+                else if(playerBox.X-120==bird1.X&& moveDirectionForBird1=="right" )
+                {
+                    shoot();
+                }
+            }
+            else
+            {
+                if (moveDirectionForBird2=="left" && (playerBox.X-bird2.X == -170))
+                {
+                    shoot();
+                }
+                else
+                {
+                    right();
+                }
+            }
+        }
+        private void countTimer_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
